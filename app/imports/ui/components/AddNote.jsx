f49733c8ee1c03a54e287553866ae847f144411f
malialiu <mliu0141@gmail.com>
2021-11-09 19:39:13 -1000

    notes-2 finished in 35 minutes.

---
+++ app/imports/ui/components/AddNote.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Segment } from 'semantic-ui-react';
+import { AutoForm, ErrorsField, HiddenField, SubmitField, TextField } from 'uniforms-semantic';
+import swal from 'sweetalert';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import PropTypes from 'prop-types';
+import SimpleSchema from 'simpl-schema';
+import { Notes } from '../../api/note/Notes';
+
+const formSchema = new SimpleSchema({
+  notes: String,
+  owner: String,
+  contactId: String,
+  createdAt: Date,
+});
+
+const bridge = new SimpleSchema2Bridge(formSchema);
+
+/** Renders the Page for adding a document. */
+class AddNote extends React.Component {
+
+  // On submit, insert the data.
+  submit(data, formRef) {
+    const { notes, owner, contactId, createdAt } = data;
+    Notes.collection.insert({ notes, owner, contactId, createdAt },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Note added successfully', 'success');
+          formRef.reset();
+        }
+      });
+  }
+
+  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
+  render() {
+    let fRef = null;
+    return (
+      <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)} >
+        <Segment>
+          <TextField label="Add a timestamped note" name='notes'/>
+          <SubmitField value='Submit'/>
+          <ErrorsField/>
+          <HiddenField name='owner' value={this.props.owner}/>
+          <HiddenField name='contactId' value={this.props.contactId}/>
+          <HiddenField name='createdAt' value={new Date()}/>
+        </Segment>
+      </AutoForm>
+    );
+  }
+}
+
+AddNote.propTypes = {
+  owner: PropTypes.string.isRequired,
+  contactId: PropTypes.string.isRequired,
+};
+
+export default AddNote;
